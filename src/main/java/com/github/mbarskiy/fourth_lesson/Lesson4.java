3b63a9e2a68ec1b12cb808c1dd0aba8e3cb471f6
Mikhail Barskiy <mikebarskiy@gmail.com>
2019-10-16 19:16:25 +0300

    [Fourth lesson] Types of ResultSet, CachedRowSet

---
+++ src/main/java/com/github/mbarskiy/fourth_lesson/Lesson4.java
@@ -0,0 +1,51 @@
+package com.github.mbarskiy.fourth_lesson;
+
+import com.github.mbarskiy.ConnectionProperties;
+
+import java.io.IOException;
+import java.sql.*;
+import java.util.Properties;
+
+public class Lesson4 {
+
+    public static void main(String[] args) throws IOException, SQLException, ClassNotFoundException {
+        Properties properties = ConnectionProperties.getConnectionProperties();
+        String url = properties.getProperty("url");
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        try (Connection conn = DriverManager.getConnection(url, properties);
+             Statement statement = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE)) {
+
+            try (ResultSet rs = statement.executeQuery("SELECT * FROM Books")) {
+//                while (rs.next()) {
+//                    int id = rs.getInt(1);
+//                    double price = rs.getDouble(3);
+//                    if (id == 4) {
+//                        rs.updateString("name", "Spartacus (discount)");
+//                        rs.updateDouble(3, price - 10);
+//                        rs.updateRow();
+//                    }
+//                }
+                if (rs.absolute(2))
+                    System.out.println(rs.getString("name"));
+                if (rs.previous())
+                    System.out.println(rs.getString("name"));
+                if (rs.last())
+                    System.out.println(rs.getString("name"));
+                if (rs.relative(-3)) {
+                    ResultSetMetaData rsmd = rs.getMetaData();
+                    while (rs.next()) {
+                        for (int i = 1; i < rsmd.getColumnCount(); i++) {
+                            String field = rsmd.getColumnName(i);
+                            String value = rs.getString(field);
+                            System.out.print(field + ": " + value + " ");
+                        }
+                        System.out.println();
+                    }
+                }
+            }
+        }
+    }
+
+}
---
+++ src/main/java/com/github/mbarskiy/fourth_lesson/RowSetLesson.java
@@ -0,0 +1,52 @@
+package com.github.mbarskiy.fourth_lesson;
+
+import com.github.mbarskiy.ConnectionProperties;
+
+import javax.sql.rowset.CachedRowSet;
+import javax.sql.rowset.RowSetFactory;
+import javax.sql.rowset.RowSetProvider;
+import java.io.IOException;
+import java.sql.*;
+import java.util.Properties;
+
+public class RowSetLesson {
+
+    public static void main(String[] args) throws SQLException, IOException, ClassNotFoundException {
+        ResultSet resultSet = getResultSet();
+//        while (resultSet.next())
+//            System.out.println(resultSet.getString("name"));
+
+        CachedRowSet rowSet = (CachedRowSet) resultSet;
+        rowSet.setCommand("SELECT * FROM Books WHERE price > ?");
+        rowSet.setDouble(1, 30);
+
+        Properties properties = ConnectionProperties.getConnectionProperties();
+        rowSet.setUrl(properties.getProperty("url"));
+        rowSet.setUsername(properties.getProperty("user"));
+        rowSet.setPassword(properties.getProperty("password"));
+        rowSet.execute();
+
+        while (rowSet.next()) {
+            String name = rowSet.getString("name");
+            double price = rowSet.getDouble(3);
+            System.out.println(name + " " + price);
+        }
+    }
+
+    private static ResultSet getResultSet() throws IOException, ClassNotFoundException, SQLException {
+        Properties properties = ConnectionProperties.getConnectionProperties();
+        String url = properties.getProperty("url");
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        try (Connection conn = DriverManager.getConnection(url, properties);
+             Statement stat = conn.createStatement()) {
+            ResultSet resultSet = stat.executeQuery("SELECT * FROM Books");
+
+            RowSetFactory factory = RowSetProvider.newFactory();
+            CachedRowSet cachedRowSet = factory.createCachedRowSet();
+            cachedRowSet.populate(resultSet);
+            return cachedRowSet;
+        }
+    }
+}
